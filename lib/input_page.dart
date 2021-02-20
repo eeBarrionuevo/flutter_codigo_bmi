f046f4d1a6ef03ec61df2f852b50f843d0e4e28d
Elvis Barrionuevo <idcareg.g@gmail.com>
2021-02-20 10:45:37 -0500

    ReusableCard Widget

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -7,19 +5,14 @@ const activeCardColor = Color(0xff1B2440);
 const bottomContainerColor = Colors.greenAccent;
 const bottomContainerHeight = 80.0;
 
-
-
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
 }
 
 class _InputPageState extends State<InputPage> {
-
-
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: Text("BMI CALCULATOR GITHUB"),
@@ -30,12 +23,46 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: [
                 Expanded(
-
-                  child: ReusableCard(color: activeCardColor,),
-
+                  child: ReusableCard(
+                    color: activeCardColor,
+                    childCard: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        FaIcon(
+                          FontAwesomeIcons.mars,
+                          size: 70.0,
+                        ),
+                        SizedBox(
+                          height: 7.0,
+                        ),
+                        Text(
+                          "MALE",
+                          style: TextStyle(fontSize: 18.0),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
                 Expanded(
-                  child:ReusableCard(color: activeCardColor,),
+                  child: ReusableCard(
+                    color: activeCardColor,
+                    childCard: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        FaIcon(
+                          FontAwesomeIcons.venus,
+                          size: 70.0,
+                        ),
+                        SizedBox(
+                          height: 7.0,
+                        ),
+                        Text(
+                          "FEMALE",
+                          style: TextStyle(fontSize: 18.0),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
               ],
             ),
@@ -44,7 +71,9 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: [
                 Expanded(
-                  child: ReusableCard(color: activeCardColor,),
+                  child: ReusableCard(
+                    color: activeCardColor,
+                  ),
                 ),
               ],
             ),
@@ -53,10 +82,14 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: [
                 Expanded(
-                  child: ReusableCard(color: activeCardColor,),
+                  child: ReusableCard(
+                    color: activeCardColor,
+                  ),
                 ),
                 Expanded(
-                  child: ReusableCard(color: activeCardColor,),
+                  child: ReusableCard(
+                    color: activeCardColor,
+                  ),
                 ),
               ],
             ),
@@ -76,33 +109,19 @@ class _InputPageState extends State<InputPage> {
 class ReusableCard extends StatelessWidget {
 
   final Color color;
+  final Widget childCard;
 
-  ReusableCard({@required this.color});
+  ReusableCard({@required this.color, @required this.childCard});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
-          color: color,
-          borderRadius: BorderRadius.circular(12.0)
+        color: color,
+        borderRadius: BorderRadius.circular(12.0),
       ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          FaIcon(FontAwesomeIcons.venus, size: 60.0,),
-          SizedBox(
-            height: 7.0,
-          ),
-          Text("MALE")
-        ],
-      ),
+      child: childCard,
     );
   }
-
 }
-
-
-
-
-
